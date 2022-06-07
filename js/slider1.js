fb25d24b0898b34c3171b98258361ca8a47ee919
OxY623 <fuzzydoorproductions0@gmail.com>
2022-06-07 20:09:54 +0300

    update

--- js/slider1.js
+++ js/slider1.js
@@ -1,46 +1,43 @@
-var sliderFactory = {
-    getNewsSlider: function(){
-        var newSlider = {
-            imagesUrl: ['img/img_a.png', 'img/img_b.jpg', 'img/img_c.jpg'],
-            currentImgIndex: 0,
-            btnShowPrev: null, // document.querySelectory('slidePrev'),
-            btnShowNext: null, //document.querySelectory('slideNext'),
-            slideImg: null, //document.querySelectory('slideImg'),
-            init: function (elId) {
-                var that = this;
-                var element = document.querySelector('#' + elId);
-                this.btnShowPrev = element.querySelector('.slidePrev');
-                this.btnShowNext = element.querySelector('.slideNext');
-                this.slideImg = element.querySelector('.slideImg');
-                this.slideImg.src = this.imagesUrl[this.currentImgIndex];
-                //subsride to events
-                this.btnShowPrev.addEventListener('click', function (x) {
-                    that.onShowBtnPrevClick(x)
-                });
-                this.btnShowNext.addEventListener("click", function (x) {
-                    that.onShowBtnNextClick(x)
-                });
-            },
-            onShowBtnPrevClick: function (x) {
-                this.currentImgIndex--;
-                this.slideImg.src = this.imagesUrl[this.currentImgIndex];
-                if (this.currentImgIndex < 0) {
-                    this.currentImgIndex = this.imagesUrl.length - 1;
-                    this.slideImg.src = this.imagesUrl[this.currentImgIndex];
-                }
-            },
+// просто запрашиваем DOM... будто просим разрешение у босса!
+var links = document.querySelectorAll(".itemLinks");
+var wrapper = document.querySelector("#wrapper");
 
-            onShowBtnNextClick: function (x) {
-                this.currentImgIndex++;
-                this.slideImg.src = this.imagesUrl[this.currentImgIndex];
-                if (this.currentImgIndex > this.imagesUrl.length - 1) {
-                    this.currentImgIndex = 0;
-                    this.slideImg.src = this.imagesUrl[this.currentImgIndex];
-                }
-            }
+// activeLink обеспечивает метку для активного элемента
+var activeLink = 0;
 
-        }
-        return newSlider;
+// устанавливаем отслеживание событий
+for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    link.addEventListener('click', setClickedItem, false);
+
+    // определяем элемент для activeLink
+    link.itemID = i;
+}
+
+// устанавливаем первый элемент в качестве активного
+links[activeLink].classList.add("active");
+
+function setClickedItem(e) {
+    removeActiveLinks();
+
+    var clickedLink = e.target;
+    activeLink = clickedLink.itemID;
+
+    changePosition(clickedLink);
+}
+
+function removeActiveLinks() {
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.remove("active");
     }
-};
+}
+
+// Обработчик изменяет позицию слайдера, после того, как мы убедились,
+// что в качестве активной обозначена нужная нам ссылка.
+function changePosition(link) {
+    link.classList.add("active");
+
+    var position = link.getAttribute("data-pos");
+    wrapper.style.left = position;
+}
 
